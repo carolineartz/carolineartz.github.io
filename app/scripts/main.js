93f37c098483bc9bccfb3d2d802b9e04655b0413
carolineartz <ceartz@gmail.com>
2015-01-24 00:14:18 -0600

    navigation working for accordion and external top navigation event target programmically triggering

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,73 @@
 /* jshint devel:true */
 /* jshint node:true */
-(function () {
+(function() {
     'use strict';
+    // var $section;
 
-    function navigateToElement(e) {
-        var $elem = $(e.target).prev('.panel-heading').parent();
-        console.log($elem);
-        $('html, body').animate({
-            scrollTop: $elem.offset().top - 30
-        }, 500);
+
+
+
+
+    // function navToElement(e) {
+    //     var $target = $(e.target);
+
+    //     if ($target.hasClass('top-navigation-link')) {
+    //         $section = $($target.attr('href')).collapse('show');
+    //     }
+    //     // else: target has class .panel-collapse
+    //     else {
+    //         $section = $target.prev('.panel-heading').parent();
+    //     }
+
+    //     $('html, body').animate({
+    //         scrollTop: $section.offset().top - 30
+    //     }, 100);
+    // }
+
+    function navToElement(e) {
+        var target = $(e.target);
+        console.log($(target));
+
+        if ($(target).hasClass('panel-collapse')) {
+            var $section = $(target).prev('.panel-heading').parent();
+            $('html, body').animate({
+                scrollTop: $section.offset().top - 30
+            }, 100);
+        } else if ($(target).hasClass('top-navigation-link')) {
+            var sectionId = $(target).attr('href');
+            $('#accordion').find('.collapse.in').removeClass('in');
+
+            var $section = $(sectionId).collapse('show');
+            $('html, body').animate({
+                scrollTop: $section.offset().top - 30
+            }, 100);
+        }
+
     }
 
-    $('#accordion').on('shown.bs.collapse', navigateToElement);
+
+
+
+
+
+
+    $('#accordion').on('shown.bs.collapse', navToElement);
+    $('.top-navigation-link, .panel-collapse').on('click', navToElement);
+
 
 })();
 
 
-$(function () {
+
+/*-----  End of On-page scrolling navigation  ------*/
+
+
+
+$(function() {
 
 
     'use strict';
-    console.log($('body').scrollTop());
+    // console.log($('body').scrollTop());
 
     if ($('body').scrollTop() === 0) {
         $('.navbar-fixed-top').css('top', '-50px');
@@ -36,7 +83,7 @@ $(function () {
     //calculate size of intro
     var iheight = $(window).height();
     $('.intro').css('height', iheight + 'px');
-    console.log(iheight + 'px');
+    // console.log(iheight + 'px');
 
     //  **************************************************************
 
@@ -49,14 +96,14 @@ $(function () {
 
     function initHeadline() {
         singleLetters($('.headline.letters').find('b')); //insert <i> element for each letter of a changing word
-        console.log($('.headline.letters').find('b'));
+        // console.log($('.headline.letters').find('b'));
         animateHeadline($('.headline'));
     }
 
     initHeadline();
 
     function singleLetters($words) {
-        $words.each(function () {
+        $words.each(function() {
             var word = $(this),
                 letters = word.text().split(''),
                 selected = word.hasClass('is-visible');
@@ -75,7 +122,7 @@ $(function () {
         if (!headline.hasClass('type')) { //assign to .cd-words-wrapper the width of its longest word
             var words = headline.find('.words-wrapper b'),
                 width = 0;
-            words.each(function () {
+            words.each(function() {
                 var wordWidth = $(this).width();
                 if (wordWidth > width) {
                     width = wordWidth;
@@ -83,7 +130,7 @@ $(function () {
             });
             headline.find('.words-wrapper').css('width', width);
         }
-        setTimeout(function () {
+        setTimeout(function() {
             hideWord(headline.find('.is-visible').eq(0));
         }, duration);
     }
@@ -94,11 +141,11 @@ $(function () {
         if ($word.parents('.headline').hasClass('type')) {
             var parentSpan = $word.parent('.words-wrapper');
             parentSpan.addClass('selected').removeClass('waiting');
-            setTimeout(function () {
+            setTimeout(function() {
                 parentSpan.removeClass('selected');
                 $word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
             }, selectionDuration);
-            setTimeout(function () {
+            setTimeout(function() {
                 showWord(nextWord, typeLettersDelay);
             }, typeAnimationDelay);
 
@@ -109,7 +156,7 @@ $(function () {
 
         } else {
             switchWord($word, nextWord);
-            setTimeout(function () {
+            setTimeout(function() {
                 hideWord(nextWord);
             }, animationDelay);
         }
@@ -126,11 +173,11 @@ $(function () {
         $letter.removeClass('in').addClass('out');
 
         if (!$letter.is(':last-child')) {
-            setTimeout(function () {
+            setTimeout(function() {
                 hideLetter($letter.next(), $word, $bool, $duration);
             }, $duration);
         } else if ($bool) {
-            setTimeout(function () {
+            setTimeout(function() {
                 hideWord(takeNext($word));
             }, animationDelay);
         }
@@ -145,17 +192,17 @@ $(function () {
         $letter.addClass('in').removeClass('out');
 
         if (!$letter.is(':last-child')) {
-            setTimeout(function () {
+            setTimeout(function() {
                 showLetter($letter.next(), $word, $bool, $duration);
             }, $duration);
         } else {
             if ($word.parents('.headline').hasClass('type')) {
-                setTimeout(function () {
+                setTimeout(function() {
                     $word.parents('.words-wrapper').addClass('waiting');
                 }, 200);
             }
             if (!$bool) {
-                setTimeout(function () {
+                setTimeout(function() {
                     hideWord($word);
                 }, animationDelay);
             }
@@ -182,10 +229,10 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 }
 
 if (!MOBILE) {
-    console.log('is not mobile');
+    // console.log('is not mobile');
     $('.mobile-sep').css('display', 'none');
 } else {
-    console.log('is is mobile');
+    // console.log('is is mobile');
     var skillsStyles = {
         backgroundAttachment: 'scroll',
         background: '#f7d100'
