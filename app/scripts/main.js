be880cbedf378867a908339925bd82b93aab7980
carolineartz <ceartz@gmail.com>
2015-02-08 01:33:30 -0600

    before main nav change

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -51,7 +51,7 @@
      */
 
     var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
-    $('#js-centered-navigation-menu').removeClass("show");
+    $('#js-centered-navigation-menu').removeClass('show');
 
     menuToggle.on('click', function(e) {
         e.preventDefault();
@@ -67,6 +67,7 @@
     $(function() {
         $.mark.jump();
     });
+
     $('.nav-link a, .panel-collapse').on('click', navToElement);
     $('#accordion').on('shown.bs.collapse', navToElement);
 
@@ -74,7 +75,7 @@
     if ($('body').scrollTop() === 0) {
         $('header').not('.nav-down').css('top', '-60px');
     } else {
-      $('header').css('top', '0')
+        $('header').css('top', '0');
     }
 
     // $('.nav-up').css('top', '-60px');
@@ -88,22 +89,21 @@ var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('header').outerHeight();
 
-$(window).scroll(function(event) {
+$(window).scroll(function() {
+    'use strict';
     didScroll = true;
 });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
+
 
 function hasScrolled() {
+    'use strict';
     var st = $(this).scrollTop();
 
     // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta) return;
+    if (Math.abs(lastScrollTop - st) <= delta) {
+        return;
+    }
 
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
@@ -120,7 +120,13 @@ function hasScrolled() {
     lastScrollTop = st;
 }
 
-
+setInterval(function() {
+    'use strict';
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
 
 
 
---
+++ app/scripts/nav-new.js
@@ -0,0 +1,10 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function() {
+    'use strict';
+    didScroll = true;
+});
