90a00b943a06088d71c9b75fb09aec0075f47c5f
carolineartz <ceartz@gmail.com>
2015-03-31 20:25:10 -0500

    update js accordion

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,354 +1,217 @@
 /* jshint node:true */
-(function() {
+(function () {
     'use strict';
 
-    function navToElement(e) {
-        var target = $(e.target);
-        var $section;
-
-        if ($(target).hasClass('panel-collapse')) {
-            $section = $(target).prev('.panel-heading').parent();
-            $('html, body').animate({
-                scrollTop: $section.offset().top
-            }, 1000, function() {
-                window.location.hash = '#' + $section.find('.panel-collapse').attr('id');
-            });
-        } else if ($(target).parent().hasClass('nav-link')) {
-            var sectionId = $(target).attr('href');
-            $(sectionId).collapse('show');
-            $('#accordion').find('.collapse.in').removeClass('in');
-$(section) = $(sectionId).collapse('show');
-            $('html, body').animate({
-                scrollTop: $(section).offset().top
-            });
-        }
-    }
-
-    // $.mark = {
-    //     jump: function(options) {
-    //         var defaults = {
-    //             selector: 'a.scroll-on-page-link'
-    //         };
-    //         if (typeof options === 'string') {
-    //             defaults.selector = options;
-    //         }
-    //         options = $.extend(defaults, options);
-    //         return $(options.selector).click(function(e) {
-    //             var jumpobj = $(this);
-    //             var target = jumpobj.attr('href');
-    //             var thespeed = 1000;
-    //             var offset = $(target).offset().top;
-    //             $('html,body').animate({
-    //                 scrollTop: offset
-    //             }, thespeed);
-    //             e.preventDefault();
-    //         });
-    //     }
-    // };
-
-
-
-
-    // $(function() {
-    //     $.mark.jump();
-    // });
-
-    $('.nav-link a, .panel-collapse').on('click', navToElement);
-    $('#accordion').on('shown.bs.collapse', navToElement);
-
-
-
+    $('.js-accordion-trigger').bind('click', function (e) {
+        // apply the toggle to the ul
+        jQuery(this).parent().toggleClass('is-expanded');
+        e.preventDefault();
+    });
 })();
 
+/*
+ *    Mobile Fix for Skills Fixed BG
+ *
+ */
 
-
-
-
-
-
-
-
-/*-----  End of On-page scrolling navigation  ------*/
-
-
-
-$(function() {
-
-
+$(function () {
     'use strict';
-
-
-
-    // $('.navbar-fixed-top').autoHidingNavbar({
-    //     'animationDuration': 300,
-    //     'showOnBottom': false
-    // });
-
-
-
-
-
-    //calculate size of intro
     var iheight = $(window).height();
     $('.intro').css('height', iheight + 'px');
-
-
-    //  **************************************************************
-
-    var animationDelay = 2500,
-        typeLettersDelay = 150,
-        selectionDuration = 500,
-        typeAnimationDelay = selectionDuration + 800;
-
-
-
-    function initHeadline() {
-        singleLetters($('.headline.letters').find('b')); //insert <i> element for each letter of a changing word
-        // console.log($('.headline.letters').find('b'));
-        animateHeadline($('.headline'));
+    var MOBILE = false;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        MOBILE = true;
     }
-
-    initHeadline();
-
-    function singleLetters($words) {
-        $words.each(function() {
-            var word = $(this),
-                letters = word.text().split(''),
-                selected = word.hasClass('is-visible');
-            for (var letter in letters) {
-                letters[letter] = (selected) ? '<i class="in">' + letters[letter] + '</i>' : '<i>' + letters[letter] + '</i>';
-            }
-            var newLetters = letters.join('');
-            word.html(newLetters).css('opacity', 1);
-        });
+    if (!MOBILE) {
+        $('.mobile-sep').css('display', 'none');
+    } else {
+        var skillsStyles = {
+            backgroundAttachment: 'scroll',
+            background: '#f7d100'
+        };
+        $('#skills .content').css(skillsStyles);
     }
-
-    function animateHeadline($headline) {
-        var duration = animationDelay;
-        var headline = $headline;
-
-        if (!headline.hasClass('type')) { //assign to .words-wrapper the width of its longest word
-            var words = headline.find('.words-wrapper b'),
-                width = 0;
-            words.each(function() {
-                var wordWidth = $(this).width();
-                if (wordWidth > width) {
-                    width = wordWidth;
-                }
-            });
-            headline.find('.words-wrapper').css('width', width);
-        }
-        setTimeout(function() {
-            hideWord(headline.find('.is-visible').eq(0));
-        }, duration);
-    }
-
-    function hideWord($word) {
-        var nextWord = takeNext($word);
-
-        if ($word.parents('.headline').hasClass('type')) {
-            var parentSpan = $word.parent('.words-wrapper');
-            parentSpan.addClass('selected').removeClass('waiting');
-            setTimeout(function() {
-                parentSpan.removeClass('selected');
-                $word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
-            }, selectionDuration);
-            setTimeout(function() {
-                showWord(nextWord, typeLettersDelay);
-            }, typeAnimationDelay);
-
-        } else if ($word.parents('.headline').hasClass('letters')) {
-            var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
-            hideLetter($word.find('i').eq(0), $word, bool, typeLettersDelay);
-            showLetter(nextWord.find('i').eq(0), nextWord, bool, typeLettersDelay);
-
-        } else {
-            switchWord($word, nextWord);
-            setTimeout(function() {
-                hideWord(nextWord);
-            }, animationDelay);
-        }
-    }
-
-    function showWord($word, $duration) {
-        if ($word.parents('.headline').hasClass('type')) {
-            showLetter($word.find('i').eq(0), $word, false, $duration);
-            $word.addClass('is-visible').removeClass('is-hidden');
-        }
-    }
-
-    function hideLetter($letter, $word, $bool, $duration) {
-        $letter.removeClass('in').addClass('out');
-
-        if (!$letter.is(':last-child')) {
-            setTimeout(function() {
-                hideLetter($letter.next(), $word, $bool, $duration);
-            }, $duration);
-        } else if ($bool) {
-            setTimeout(function() {
-                hideWord(takeNext($word));
-            }, animationDelay);
-        }
-
-        if ($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
-            var nextWord = takeNext($word);
-            switchWord($word, nextWord);
-        }
-    }
-
-    function showLetter($letter, $word, $bool, $duration) {
-        $letter.addClass('in').removeClass('out');
-
-        if (!$letter.is(':last-child')) {
-            setTimeout(function() {
-                showLetter($letter.next(), $word, $bool, $duration);
-            }, $duration);
-        } else {
-            if ($word.parents('.headline').hasClass('type')) {
-                setTimeout(function() {
-                    $word.parents('.words-wrapper').addClass('waiting');
-                }, 200);
-            }
-            if (!$bool) {
-                setTimeout(function() {
-                    hideWord($word);
-                }, animationDelay);
-            }
-        }
-    }
-
-    /// UTILITIES
-    function takeNext($word) {
-        return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
-    }
-
-    function switchWord($oldWord, $newWord) {
-        $oldWord.removeClass('is-visible').addClass('is-hidden');
-        $newWord.removeClass('is-hidden').addClass('is-visible');
-    }
-
-
 });
 
+/*
+ *    Top Navigation- Refills
+ *
+ */
 
-// ****************************************************************
-var MOBILE = false;
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    var MOBILE = true;
-}
-
-if (!MOBILE) {
-    // console.log('is not mobile');
-    $('.mobile-sep').css('display', 'none');
-} else {
-    // console.log('is is mobile');
-    var skillsStyles = {
-        backgroundAttachment: 'scroll',
-        background: '#f7d100'
-    };
-    $('#skills .content').css(skillsStyles);
-
-}
-
-
-
-
-    /*
-     *    Top Navigation- Refills
-     *
-     */
-
+$(function () {
+    'use strict';
     var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
-
-
-    menuToggle.on('click', function(e) {
+    menuToggle.on('click', function (e) {
         e.preventDefault();
-        $('#js-centered-navigation-menu').slideToggle(function() {
+        $('#js-centered-navigation-menu').slideToggle(function () {
             if ($('#js-centered-navigation-menu').is(':hidden')) {
                 $('#js-centered-navigation-menu').removeAttr('style');
             }
         });
     });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #######################################################################################
-
-
-
-
-
-
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-var $window = $(window);
-
-$(window).scroll(function() {
+$(function () {
     'use strict';
-    didScroll = true;
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+    var $window = $(window);
+    $(window).scroll(function () {
+        // 'use strict';
+        didScroll = true;
+    });
+
+    function hasScrolled() {
+        // 'use strict';
+        var st = $window.scrollTop();
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta) {
+            return;
+        }
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            $('header').removeClass('nav-down').addClass('nav-up');
+            if ($('#js-centered-navigation-menu').not(':hidden')) {
+                $('#js-centered-navigation-menu').removeAttr('style');
+            }
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                $('header').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+        lastScrollTop = st;
+    }
+    setInterval(function () {
+        // 'use strict';
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 400);
+    (function () {
+        // 'use strict';
+        if ($('body').scrollTop() === 0) {
+            $('header').not('.nav-down').css('top', '-60px');
+        } else {
+            $('header').css('top', '0');
+        }
+    })();
 });
 
 
-function hasScrolled() {
-    'use strict';
-    var st = $window.scrollTop();
 
 
-    // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta) {
-        return;
-    }
+/*=================================
+=            scrollspy            =
+=================================*/
 
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-        if ($('#js-centered-navigation-menu').not(':hidden')) {
-                $('#js-centered-navigation-menu').removeAttr('style');
-            }
-    } else {
-        // Scroll Up
-        if (st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
-    }
+// Cache selectors
+var lastId,
+    topMenu = $("#js-navigation-menu"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
 
-    lastScrollTop = st;
-}
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({
+      scrollTop: offsetTop
+  }, 500);
+  e.preventDefault();
+});
 
-setInterval(function() {
-    'use strict';
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
 
-(function() {
-      'use strict';
-    if ($('body').scrollTop() === 0) {
-        $('header').not('.nav-down').css('top', '-60px');
-    } else {
-        $('header').css('top', '0');
-    }
-})();
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }
+});
+/*-----  End of scrollspy  ------*/
+
+$(function() {
+  $('#accordion').accordion({
+    oneOpenedItem : true
+  });
+});
+
+
+
+
+
+
+
+
+/*
+ *
+ *          TYPING INTRO
+ *
+ */
+
+/*-----  End of Section comment block  ------*/
+
+// Instantiation
+// var theater = new TheaterJS();
+
+// // Describe actors
+// theater
+//     .describe("Intro", .8, "#typing");
+
+// // Write the scenario
+// theater
+//     .write("Intro:Full Stack Web Developer", 600)
+//     .write(400)
+//     .write("Intro:UX/UI Engineer", 600)
+//     .write(function () {
+//         // loop on the scenario
+//         this.play(true);
+//     })
+//     .write(400)
+//     .play(true);
+
+// // Listen to theater's events
+// theater
+//     .on("say:start, erase:start", function () {
+//         var self = this,
+
+//             current = self.current.voice;
+
+//         self.utils.addClass(current, "saying");
+//     })
+//     .on("say:end, erase:end", function () {
+//         // remove blinking caret
+//         // self.utils.addClass(current, "saying");
+//     })
+//     .on("*", function () {
+
+//     });
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,160 +3,182 @@
 // generated on 2015-01-09 using generator-gulp-webapp 0.2.0
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-
+var uncss = require('gulp-uncss');
 
 gulp.task('styles', function () {
-  //var replace = require('gulp-replace');
-  return gulp.src('app/styles/main.scss')
-    .pipe($.plumber())
-    .pipe($.rubySass({
-      style: 'expanded',
-      precision: 10
-    }))
-    //.pipe(replace(/..\/fonts/gm, 'fonts'))
-    .pipe($.autoprefixer({browsers: ['last 1 version']}))
-    .pipe(gulp.dest('.tmp/styles'));
+    //var replace = require('gulp-replace');
+    return gulp.src('app/styles/main.scss')
+        .pipe($.plumber())
+        .pipe($.rubySass({
+            style: 'expanded',
+            precision: 10
+        }))
+        //.pipe(replace(/..\/fonts/gm, 'fonts'))
+        .pipe($.autoprefixer({
+            browsers: ['last 1 version']
+        }))
+        .pipe(gulp.dest('.tmp/styles'));
+    // Concatenate And Minify Styles
+    //.pipe($.if('*.css', $.csso()))
+    //.pipe(gulp.dest('dist/styles'))
+    //.pipe($.size({
+    //    title: 'styles'
+    //}));
 });
 
 gulp.task('jshint', function () {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'));
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('templates', function() {
-  var jade = require('gulp-jade');
-  return gulp.src('app/templates/*.jade')
-    .pipe($.plumber())
-    .pipe($.jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('app'));
+gulp.task('templates', function () {
+    var jade = require('gulp-jade');
+    return gulp.src('app/templates/*.jade')
+        .pipe($.plumber())
+        .pipe($.jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('app'));
 });
 
 gulp.task('html', ['styles'], function () {
-  var lazypipe = require('lazypipe');
-  var cssChannel = lazypipe()
-    .pipe($.csso)
-    .pipe($.replace, 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap','fonts');
-  var assets = $.useref.assets({searchPath: '{.tmp,app}'});
+    var lazypipe = require('lazypipe');
+    var cssChannel = lazypipe()
+        .pipe($.csso)
+    var assets = $.useref.assets({
+        searchPath: '{.tmp,app}'
+    });
 
-  return gulp.src('app/*.html')
-    .pipe(assets)
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', cssChannel()))
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe(gulp.dest('dist'));
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe($.if('*.js', $.uglify()))
+        // Remove Any Unused CSS
+        .pipe($.if('*.css', $.uncss({
+            html: [
+                'app/index.html'
+            ]
+            // CSS Selectors for UnCSS to ignore
+            //ignore: [
+            //]
+        })))
+        // Concatenate And Minify Styles
+        .pipe($.if('*.css', $.csso()))
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('images', function () {
-  return gulp.src('app/images/**/*')
-   .pipe($.imagemin({
-      progressive: true,
-      interlaced: true
-    }))
-    .pipe(gulp.dest('dist/images'));
+    return gulp.src('app/images/**/*')
+        .pipe($.imagemin({
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('docs', function () {
-  return gulp.src('app/docs/**/*')
-    .pipe(gulp.dest('dist/docs'));
+    return gulp.src('app/docs/**/*')
+        .pipe(gulp.dest('dist/docs'));
 });
 
-gulp.task('dbc-files', function() {
-  return gulp.src('app/dbc-portfolio/**/*')
-    .pipe(gulp.dest('dist/dbc-portfolio'));
-  })
+gulp.task('dbc-files', function () {
+    return gulp.src('app/dbc-portfolio/**/*')
+        .pipe(gulp.dest('dist/dbc-portfolio'));
+})
 
 gulp.task('fonts', function () {
-  return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
-    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-    .pipe($.flatten())
-    .pipe(gulp.dest('dist/fonts'));
+    return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
+        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('extras', function () {
-  return gulp.src([
-    'app/*.*',
-    '!app/*.html',
-    'node_modules/apache-server-configs/dist/.htaccess'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist'));
+    return gulp.src([
+        'app/*.*',
+        '!app/*.html',
+        'node_modules/apache-server-configs/dist/.htaccess'
+    ], {
+        dot: true
+    }).pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 gulp.task('connect', ['styles'], function () {
-  var serveStatic = require('serve-static');
-  var serveIndex = require('serve-index');
-  var app = require('connect')()
-    .use(require('connect-livereload')({port: 35729}))
-    .use(serveStatic('.tmp'))
-    .use(serveStatic('app'))
-    // paths to bower_components should be relative to the current file
-    // e.g. in app/index.html you should use ../bower_components
-    .use('/bower_components', serveStatic('bower_components'))
-    .use(serveIndex('app'));
+    var serveStatic = require('serve-static');
+    var serveIndex = require('serve-index');
+    var app = require('connect')()
+        .use(require('connect-livereload')({
+            port: 35729
+        }))
+        .use(serveStatic('.tmp'))
+        .use(serveStatic('app'))
+        // paths to bower_components should be relative to the current file
+        // e.g. in app/index.html you should use ../bower_components
+        .use('/bower_components', serveStatic('bower_components'))
+        .use(serveIndex('app'));
 
-  require('http').createServer(app)
-    .listen(9000)
-    .on('listening', function () {
-      console.log('Started connect web server on http://localhost:9000');
-    });
+    require('http').createServer(app)
+        .listen(9000)
+        .on('listening', function () {
+            console.log('Started connect web server on http://localhost:9000');
+        });
 });
 
 gulp.task('serve', ['connect', 'watch'], function () {
-  require('opn')('http://localhost:9000');
+    require('opn')('http://localhost:9000');
 });
 
-
-
 var options = {
-  remoteUrl: "https://github.com/carolineartz/carolineartz.github.io",
-  branch: "master"
+    remoteUrl: "https://github.com/carolineartz/carolineartz.github.io",
+    branch: "master"
 };
-  gulp.task('deploy', function () {
-    var deploy      = require('gulp-gh-pages');
-      gulp.src(["dist/**/*.*", "dist/CNAME"])
-          .pipe(deploy(options));
-  });
+gulp.task('deploy', function () {
+    var deploy = require('gulp-gh-pages');
+    gulp.src(["dist/**/*.*", "dist/CNAME"])
+        .pipe(deploy(options));
+});
 
 // inject bower components
 gulp.task('wiredep', function () {
-  var wiredep = require('wiredep').stream;
+    var wiredep = require('wiredep').stream;
 
-  gulp.src('app/styles/*.scss')
-    .pipe(wiredep())
-    .pipe(gulp.dest('app/styles'));
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep())
+        .pipe(gulp.dest('app/styles'));
 
-  gulp.src('app/*.html')
-    .pipe(wiredep({exclude: ['bootstrap-sass-official']}))
-    .pipe(gulp.dest('app'));
+    gulp.src('app/*.html')
+        .pipe(wiredep())
+        .pipe(gulp.dest('app'));
 });
 
 gulp.task('watch', ['connect'], function () {
-  $.livereload.listen();
+    $.livereload.listen();
 
-  // watch for changes
-  gulp.watch([
-    'app/*.html',
-    '.tmp/styles/**/*.css',
-    'app/scripts/**/*.js',
-    'app/images/**/*'
-  ]).on('change', $.livereload.changed);
+    // watch for changes
+    gulp.watch([
+        'app/*.html',
+        '.tmp/styles/**/*.css',
+        'app/scripts/**/*.js',
+        'app/images/**/*'
+    ]).on('change', $.livereload.changed);
 
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch('app/templates/**/*.jade', ['templates']);
-  gulp.watch('bower.json', ['wiredep']);
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch('app/templates/**/*.jade', ['templates']);
+    gulp.watch('bower.json', ['wiredep']);
 });
 
-gulp.task('build', ['jshint', 'templates', 'html', 'images', 'fonts', 'extras', 'docs', 'dbc-files'], function () {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+gulp.task('build', ['jshint', 'templates', 'styles', 'html', 'images', 'fonts', 'extras', 'docs', 'dbc-files'], function () {
+    return gulp.src('dist/**/*').pipe($.size({
+        title: 'build',
+        gzip: true
+    }));
 });
 
 gulp.task('default', ['clean'], function () {
-  gulp.start('build');
+    gulp.start('build');
 });
